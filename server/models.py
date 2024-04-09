46e861c5356079f2305dc593bca44a0b48691eff
manoti3 <hillarymanoti0@gmail.com>
2024-04-09 22:48:08 +0300

    competed assignment

--- server/models.py
+++ server/models.py
@@ -12,48 +12,62 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Hero(db.Model, SerializerMixin):
-    __tablename__ = 'heroes'
+    _tablename_ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
 
     # add relationship
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='hero', cascade='all, delete-orphan'
+    )
 
     # add serialization rules
+    serialize_rules = ('-hero_powers.hero',)
 
-    def __repr__(self):
+    def _repr_(self):
         return f'<Hero {self.id}>'
 
 
 class Power(db.Model, SerializerMixin):
-    __tablename__ = 'powers'
+    _tablename_ = 'powers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
 
     # add relationship
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='power', cascade='all, delete-orphan'
+    )
 
     # add serialization rules
+    serialize_rules = ('-hero_powers.power',)
 
     # add validation
 
-    def __repr__(self):
+    def _repr_(self):
         return f'<Power {self.id}>'
 
 
 class HeroPower(db.Model, SerializerMixin):
-    __tablename__ = 'hero_powers'
+    _tablename_ = 'hero_powers'
 
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String, nullable=False)
 
     # add relationships
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
 
     # add serialization rules
-
+    serialize_rules = ()
+    
     # add validation
 
-    def __repr__(self):
+    def _repr_(self):
         return f'<HeroPower {self.id}>'
